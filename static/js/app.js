75e15630d6f7c8d69316f83dacb96b5ad581fb15
Thomas Krapp <t.krapp@gmail.com>
2016-11-17 21:15:42 +0100

    First test version of timeseries

--- static/js/app.js
+++ static/js/app.js
@@ -1068,6 +1068,16 @@ detectIDB(function (idb_capability) {
             [ // day without break
                 {'timestamp': moment("2016-07-22 06:30"), 'type': TYPE_COMING},
                 {'timestamp': moment("2016-07-22 14:00"), 'type': TYPE_LEAVING}
+            ],
+            [ // 10h day without break
+                {'timestamp': moment("2016-07-22 06:30"), 'type': TYPE_COMING},
+                {'timestamp': moment("2016-07-22 16:30"), 'type': TYPE_LEAVING}
+            ],
+            [ // strange break
+                {'timestamp': moment("2016-07-22 06:30"), 'type': TYPE_COMING},
+                {'timestamp': moment("2016-07-22 13:00"), 'type': TYPE_LEAVING},
+                {'timestamp': moment("2016-07-22 13:40"), 'type': TYPE_COMING},
+                {'timestamp': moment("2016-07-22 16:30"), 'type': TYPE_LEAVING}
             ]
         ],
         idx_ts, idx_bk, booking, timeseries, valid, check_result, ts, prev_ts;
@@ -1101,13 +1111,26 @@ detectIDB(function (idb_capability) {
         return hours + ':' + minutes;
     };
     
-    function validateTimePair (pair) {
-        
+    function sixHoursCheck (pair) {
+        if (pair.type == TYPE_ON_BREAK) {
+            return false;
+        } else {
+            return (pair.end - pair.start) >= BREAK_AFTER;
+        }
+    };
+    
+    function nineHoursCheck (pair, time_working, time_on_break) {
+        if (pair.type == TYPE_ON_BREAK) {
+            return false;
+        } else {
+            return (time_working + (pair.end - pair.start) > SECOND_BREAK_AFTER &&
+                time_on_break < (FIRST_BREAK + SECOND_BREAK));
+        }
     };
     
     function calcTimes (timeseries) {
         var pairs = [],
-            idx_bk, pair_start, pair_end;
+            error, idx_bk, idx_p, pair, pair_start, pair_end;
         
         for (idx_bk = 0; idx_bk < timeseries.length; idx_bk += 2) {
             pair_start = timeseries[idx_bk].timestamp;
@@ -1130,10 +1153,97 @@ detectIDB(function (idb_capability) {
             });
         }
         
+        var time_on_break = 0,
+            time_working = 0;
+
+        for (idx_p = 0; idx_p < pairs.length; idx_p += 1) {
+            pair = pairs[idx_p];
+            
+            if (sixHoursCheck(pair)) {
+                pair.synthetic = true;
+                pair_end = pair.end.clone();
+                pair.end = pair.start.clone().add(BREAK_AFTER);
+
+                var duration_break = (time_on_break < FIRST_BREAK ?
+                        FIRST_BREAK - time_on_break : SECOND_BREAK),
+                    new_pair = {
+                        'start': pair.start.clone().add(BREAK_AFTER),
+                        'end': pair.start.clone().add(BREAK_AFTER).add(duration_break),
+                        'type': TYPE_ON_BREAK,
+                        'synthetic': true
+                    };
+
+                if (new_pair.end >= pair_end) {
+                    new_pair.end = pair_end;
+
+                    pairs.splice(idx_p + 1, 0, new_pair);
+                } else if (new_pair.end < pair_end) {
+                    pairs.splice(
+                        idx_p + 1,
+                        0,
+                        new_pair,
+                        {
+                            'start': new_pair.end.clone(),
+                            'end': pair_end,
+                            'type': TYPE_WORKING,
+                            'synthetic': true
+                        }
+                    );
+                }
+            } else if (nineHoursCheck(pair, time_working, time_on_break)) {
+                var new_pair = {
+                        'start': pair.start.clone()
+                            .add(SECOND_BREAK_AFTER - time_working),
+                        'end': pair.start.clone()
+                            .add(SECOND_BREAK_AFTER - time_working).add(SECOND_BREAK),
+                        'type': TYPE_ON_BREAK,
+                        'synthetic': true
+                    },
+                    pair_end = pair.end.clone();
+
+                pair.synthetic = true;
+                pair.end = new_pair.start.clone();
+                
+                if (new_pair.end >= pair_end) {
+                    new_pair.end = pair_end;
+
+                    pairs.splice(idx_p + 1, 0, new_pair);
+                } else if (new_pair.end < pair_end) {
+                    var new_new_pair = {
+                            'start': new_pair.end.clone(),
+                            'end': pair_end,
+                            'type': TYPE_WORKING,
+                            'synthetic': true
+                        };
+                    
+                    pairs.splice(
+                        idx_p + 1,
+                        0,
+                        new_pair,
+                        new_new_pair
+                    );
+                }
+            }
+            
+            if (pair.type === TYPE_WORKING) {
+                time_working += (pair.end - pair.start);
+            } else {
+                time_on_break += (pair.end - pair.start);
+            }
+        }
+        
+        console.log('PAIRS');
+        for (idx_p = 0; idx_p < pairs.length; idx_p += 1) {
+            pair = pairs[idx_p];
+            console.log(pair.start.toString(), pair.end.toString(),
+                pair.type, pair.synthetic, pair.hula);
+        }
+        
         return pairs;
     };
     
     for (idx_ts = 0; idx_ts < timeserieses.length; idx_ts += 1) {
+        console.log('new timeseries');
         timeseries = timeserieses[idx_ts];
         check_result = checkTimeSeriesValidity(timeseries);
         valid = check_result === -1;
@@ -1145,13 +1255,8 @@ detectIDB(function (idb_capability) {
             var time_working = 0,
                 time_on_break = 0;
             
-            console.log(pairs);
             for (idx_pair = 0; idx_pair < pairs.length; idx_pair += 1) {
                 pair = pairs[idx_pair];
-                
-                console.log(pair.start.format('HH:mm'), pair.end.format('HH:mm'));
-                console.log(getReadableDiff(pair.end.diff(pair.start)));
-                console.log(getReadableDiff(BREAK_AFTER));
                 
                 if (pair.type === TYPE_WORKING) {
                     time_working += pair.end.diff(pair.start);
